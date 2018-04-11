3d2f488a6aeba3ee969fd5d3bdf4cc1a5f453981
EvilWolve <derboesewolfi@googlemail.com>
2018-04-11 23:55:50 +0300

    Added sprite renderer

---
+++ Assets/Scripts/Framework/Rendering/SpriteInstanceRenderer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Unity.Entities;
+using UnityEngine;
+
+namespace Framework.Rendering
+{
+    [Serializable]
+    public struct SpriteInstanceRenderer : ISharedComponentData
+    {
+        public Texture2D sprite;
+        public Material material;
+    }
+    public class SpriteInstanceRendererComponent : SharedComponentDataWrapper<SpriteInstanceRenderer> { }
+}
